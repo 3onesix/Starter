aa6758674d79bf4f7d87609c5db2f8e39b5f08ad
jamesfinley <jamesfinley@gmail.com>
2011-06-25 12:37:27 -0500

    Added .field wrapper to module checkboxes. Added disabled status to required modules for page template.

--- application/views/admin/pages/_sidebar.php
+++ application/views/admin/pages/_sidebar.php
@@ -1,6 +1,6 @@
 <div id="sidebar">
 	<h2>Modules</h2>
-	<input type="checkbox" name="page_[modules][seo]" value="1" checked="checked" /> <label for="">SEO</label><br />
-	<input type="checkbox" name="page_[modules][articles]" value="1" checked="checked" /> <label for="">Articles</label><br />
-	<input type="checkbox" name="page_[modules][googleanalytics]" value="1" /> <label for="">Google Analytics</label><br />
+	<div class="field"><input type="checkbox" name="page_[modules][seo]" value="1" checked="checked" disabled="disabled" /> <label for="" class="disabled">SEO</label></div>
+	<div class="field"><input type="checkbox" name="page_[modules][articles]" value="1" checked="checked" disabled="disabled" /> <label for="" class="disabled">Articles</label></div>
+	<div class="field"><input type="checkbox" name="page_[modules][googleanalytics]" value="1" /> <label for="">Google Analytics</label></div>
 </div>
