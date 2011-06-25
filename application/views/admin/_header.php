771157267f495427bb6e552863fe130319c7e181
jamesfinley <jamesfinley@gmail.com>
2011-06-25 13:30:44 -0500

    Added header

--- application/views/admin/_header.php
+++ application/views/admin/_header.php
@@ -11,6 +11,10 @@
 		<!-- Adding "maximum-scale=1" fixes the Mobile Safari auto-zoom bug: http://filamentgroup.com/examples/iosScaleBug/ -->
 	</head>
 	<body>
+		<header>
+			<h1><?=$this->config->item('starter_product_name')?></h1>
+			<div class="global"><a href="<?=site_url('admin/settings')?>">Settings</a> &bull; <a href="<?=site_url('admin/signout')?>">Signout</a></div>
+		</header>
 		<div class="container">
 			<ul id="navigation">
 				<li><a href="<?=site_url('admin')?>">Dashboard</a></li>
