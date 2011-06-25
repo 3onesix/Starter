04ed1f99833cbea37e5a8a6fe796569ecba4107c
jamesfinley <jamesfinley@gmail.com>
2011-06-25 12:51:04 -0500

    Cleaned up .field style, adding .field.checkbox.

--- application/views/admin/pages/_sidebar.php
+++ application/views/admin/pages/_sidebar.php
@@ -1,6 +1,6 @@
 <div id="sidebar">
 	<h2>Modules</h2>
-	<div class="field"><input type="checkbox" name="page_[modules][seo]" value="1" checked="checked" disabled="disabled" /> <label for="" class="disabled">SEO</label></div>
-	<div class="field"><input type="checkbox" name="page_[modules][articles]" value="1" checked="checked" disabled="disabled" /> <label for="" class="disabled">Articles</label></div>
-	<div class="field"><input type="checkbox" name="page_[modules][googleanalytics]" value="1" /> <label for="">Google Analytics</label></div>
+	<div class="field checkbox"><input type="checkbox" name="page_[modules][seo]" value="1" checked="checked" disabled="disabled" /> <label for="" class="disabled">SEO</label></div>
+	<div class="field checkbox"><input type="checkbox" name="page_[modules][articles]" value="1" checked="checked" disabled="disabled" /> <label for="" class="disabled">Articles</label></div>
+	<div class="field checkbox"><input type="checkbox" name="page_[modules][googleanalytics]" value="1" /> <label for="">Google Analytics</label></div>
 </div>
