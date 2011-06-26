76f4675fc662b91360c6bfed0ece51e15ebf5bb4
Tom Krush <t.krush@gmail.com>
2011-06-26 16:24:50 -0500

    Updated to newest Jot. This version of jot includes an updated migrations library.

--- application/controllers/Migrations.php
+++ application/controllers/Migrations.php
@@ -6,38 +6,28 @@ class Migrations extends CI_Controller
 		parent::__construct();
 
 		$this->load->database();
-		$this->load->helper(array('directory','migrations_helper'));
-
-		// Setup Migrations
-		migrations_directory_setup();
+		$this->load->helper(array('directory','jot_migrations'));	
 	}
 	
 	function index()
-	{	
-		migration_up();
-		
+	{
+		$migrations = new JotMigrations();
+		$migrations->up();
+					
 		echo 'Migrations run';
 	}
 	
 	function seed()
 	{
-		include(db_path().'seed.php');
+		$migrations = new JotMigrations();
+		$migrations->seed();
 	}
 	
 	function reset()
-	{
-		$query = $this->db->query("SHOW TABLES");
+	{		
+		$migrations = new JotMigrations();
+		$migrations->reset(TRUE);
 
-		foreach ($query->result_array() as $row) {
-			$name = current($row);
-			
-			$this->db->query("DROP TABLE ".$name);
-		}
-		
-		migration_up();
-		
-		$this->seed();
-		
 		echo 'Reset Run';
 	}
 	
@@ -48,7 +38,8 @@ class Migrations extends CI_Controller
 	
 	function create($path)
 	{
-		create_migration($path);
+		$migrations = new JotMigrations();
+		$migrations->create($path);
 
 		$CI =& get_instance();
 		$CI->load->helper('url');
