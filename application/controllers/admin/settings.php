2196bf882f689076b3c265435b50e0628603d2b2
jamesfinley <jamesfinley@gmail.com>
2011-06-26 19:00:05 -0500

    Added settings for Site Name and Site Author. If there are other SEO related settings you'd like to add, please do so.

--- application/controllers/admin/settings.php
+++ application/controllers/admin/settings.php
@@ -6,6 +6,16 @@ class Settings extends MY_Controller
 	
 	public function action_index($page = 0)
 	{
+		$data = $this->input->post('settings');
+		
+		if ($data) {
+			foreach ($data as $key => $value) {
+				setting($key, $value);
+			}
+			flash('notice', 'Settings have been saved.');
+			redirect('admin/settings');
+		}
+		
 		$this->load->view('admin/settings/index');
 	}
 }
--- application/views/admin/settings/index.php
+++ application/views/admin/settings/index.php
@@ -6,4 +6,23 @@
 	<h2>Settings</h2>
 </div>
 
+<form method="post" action="<?=site_url('admin/settings')?>">
+	<div id="page_variables">
+		<fieldset>
+			<legend>Site Info</legend>
+			<div class="field">
+				<label for="settings_site_name_field">Name</label>
+				<input type="text" name="settings[site_name]" id="settings_site_name_field" value="<?=setting('site_name')?>" />
+			</div>
+			<div class="field">
+				<label for="settings_site_author_field">Author</label>
+				<input type="text" name="settings[site_author]" id="settings_site_author_field" value="<?=setting('site_author')?>" />
+			</div>
+		</fieldset>
+	</div>
+	<div class="actions">
+		<?php echo submit_tag(); ?>
+	</div>
+</form>
+
 <?php $this->load->view('admin/_footer'); ?>
