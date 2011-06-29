e1ddd8a85555c1909524c47c57683e614320b71f
jamesfinley <jamesfinley@gmail.com>
2011-06-28 20:28:46 -0500

    Added type and label to settings Made settings in sidebar show modules with settings

--- application/db/migrate/20110625154358_CreateTableSettings.php
+++ application/db/migrate/20110625154358_CreateTableSettings.php
@@ -5,6 +5,8 @@ class CreateTableSettings
 	{
 		create_table('settings', array(
 			array('name' => 'module_id', 'type' => 'integer'),
+			array('name' => 'type', 'type' => 'string'),
+			array('name' => 'label', 'type' => 'string'),
 			array('name' => 'key', 'type' => 'string'),
 			array('name' => 'value', 'type' => 'string'),
 			MIGRATION_TIMESTAMPS
--- application/views/admin/settings/_sidebar.php
+++ application/views/admin/settings/_sidebar.php
@@ -3,6 +3,18 @@
 	<li><a href="<?=site_url('admin/settings')?>"<?=($this->uri->segment(3) == '' ? ' class="selected"' : '')?>>Meta Info</a></li>
 	<li><a href="<?=site_url('admin/settings/users')?>"<?=($this->uri->segment(3) == 'users' ? ' class="selected"' : '')?>>Users</a></li>
 	<li><a href="<?=site_url('admin/settings/modules')?>"<?=($this->uri->segment(3) == 'modules' && $this->uri->segment(4) == '' ? ' class="selected"' : '')?>>Manage Modules</a></li>
-	<li class="header">Modules</li>
-	<li><a href="<?=site_url('admin/settings/modules/starter_articles')?>"<?=($this->uri->segment(3) == 'modules' && $this->uri->segment(4) == 'starter_articles' ? ' class="selected"' : '')?>>Articles</a></li>
+	
+	<?php
+		$this->db->order_by('name');
+		$all = $this->module_model->all();
+		$modules = array();
+		foreach ($all as $m) if ($m->settings->count()) $modules[] = $m;
+	?>
+	<?php if (count($modules)): ?>
+		<li class="header">Modules</li>
+		<?php foreach ($modules as $module): ?>
+			<li><a href="<?=site_url('admin/settings/modules/'.$module->id)?>"<?=($this->uri->segment(3) == 'modules' && $this->uri->segment(4) == $module->id ? ' class="selected"' : '')?>><?=$module->name?></a></li>
+		<?php endforeach; ?>
+	<?php endif; ?>
+	
 </ul>
