cca1e77987ea1d74602281c74fc408f6ad93146b
jamesfinley <jamesfinley@gmail.com>
2011-06-28 21:25:24 -0500

    Can now save module settings

--- application/views/admin/settings/module.php
+++ application/views/admin/settings/module.php
@@ -6,7 +6,7 @@
 	<h2>Settings</h2>
 </div>
 
-<form method="post" action="<?=site_url('admin/settings')?>">
+<form method="post" action="<?=site_url('admin/settings/modules/'.$module->id)?>">
 	<?php $this->load->view('admin/settings/_sidebar.php'); ?>
 	<div id="page_variables">
 		<fieldset>
@@ -14,7 +14,7 @@
 			<?php foreach ($module->settings->all() as $setting): ?>
 				<div class="field<?=($setting->type == 'checkbox' ? ' checkbox' : '')?>">
 					<?php if ($setting->type == 'checkbox'): ?>
-						<input type="checkbox" id="settings_<?=$setting->id?>_field" name="setting[<?=$setting->id?>]" value="1" <?=($setting->value == 1 ? 'checked="checked"' : '')?> />
+						<input type="hidden" name="setting[<?=$setting->id?>]" value="0" /><input type="checkbox" id="settings_<?=$setting->id?>_field" name="setting[<?=$setting->id?>]" value="1" <?=($setting->value == 1 ? 'checked="checked"' : '')?> />
 					<?php endif; ?>
 					<label for="settings_<?=$setting->id?>_field"><?=($setting->label ? $setting->label : $setting->key)?></label>
 					<?php if (! in_array($setting->type, array('checkbox', 'list'))): ?>
