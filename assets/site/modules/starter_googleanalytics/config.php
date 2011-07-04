b1a469d0976a3562f148def5bb931c3631334c2c
jamesfinley <jamesfinley@gmail.com>
2011-07-04 09:41:07 -0500

    Added description to GA config

--- assets/site/modules/starter_googleanalytics/config.php
+++ assets/site/modules/starter_googleanalytics/config.php
@@ -3,6 +3,7 @@
 $this->module 						= array(
 	'name'							=> 'Google Analytics',
 	'simple_name'					=> 'starter_googleanalytics',
+	'description'					=> 'The Google Analytics module provides a quick and easy way to install analytics on all your pages, but also to track your numbers for any page and for your site within Starter.',
 	'version'						=> '1.0',
 	'settings'						=> array(
 		array(
