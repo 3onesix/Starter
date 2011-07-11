5ba5c083065dcccf8baa359d756de2d59085a8e5
James Finley <jamesfinley@gmail.com>
2011-07-11 10:19:29 -0500

    Now can load scripts into footer

--- application/views/admin/_footer.php
+++ application/views/admin/_footer.php
@@ -5,5 +5,10 @@
 		<script src="/assets/shared/js/jquery-1.6.1.min.js"></script>
 		<script src="/assets/shared/js/jquery.easing.1.3.js"></script>
 		<script src="/assets/app/js/app.js"></script>
+		<?php if (isset($scripts)): ?>
+		<?php foreach($scripts as $script): ?>
+			<script src="<?=$script?>" type="text/javascript"></script>
+		<?php endforeach; ?>
+		<?php endif; ?>
 	</body>
 </html>
