f609237fc9cae1b262f0ec77fc7b17a6dcf6bf9b
James Finley <jamesfinley@gmail.com>
2011-09-20 10:51:52 -0500

    Styled login page

--- application/views/admin/account/signin.php
+++ application/views/admin/account/signin.php
@@ -8,9 +8,12 @@
 		<![endif]-->
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 		<!-- Adding "maximum-scale=1" fixes the Mobile Safari auto-zoom bug: http://filamentgroup.com/examples/iosScaleBug/ -->
+		
+		<link rel="stylesheet" href="<?=base_url()?>assets/app/css/login.css" type="" />
 	</head>
 	<body>
 		<form id="signin" method="post" action="<?=site_url('admin/authenticate')?>">
+			<h1 class="image" style="background-image: url(<?=site_url($this->config->item('starter_product_image'))?>)"><a href="<?=site_url('admin')?>"><?=$this->config->item('starter_product_name')?></a></h1>
 			<div class="field">
 				<label for="username_field">Username<label>
 				<input type="text" name="username" id="username_field" />
