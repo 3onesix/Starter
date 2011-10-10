a40d0b1faaf66c156ce9b09458892ac69ea59d58
James Finley <jamesfinley@gmail.com>
2011-10-10 18:59:48 -0500

    Added route

--- application/config/routes.php
+++ application/config/routes.php
@@ -14,6 +14,7 @@ $route['admin/(:any)/update/(:num)']	= "admin/$1/action_update/$2";
 $route['admin/(:any)/edit/(:num)']		= "admin/$1/action_edit/$2";
 $route['admin/(:any)/create']	 		= "admin/$1/action_create";
 $route['admin/(:any)/new']	 			= "admin/$1/action_new";
+$route['admin/(:any)/(:any)/(:any)/(:any)/(:any)']	= "admin/$1/action_$2/$3/$4/$5";
 $route['admin/(:any)/(:any)/(:any)/(:any)']	= "admin/$1/action_$2/$3/$4";
 $route['admin/(:any)/(:any)/(:any)']	= "admin/$1/action_$2/$3";
 $route['admin/(:any)/(:any)']			= "admin/$1/action_$2";
