3080bc8294d7246bbf9e98594534a9d66ea94014
James Finley <jamesfinley@gmail.com>
2011-10-27 10:15:11 -0500

    Fix.

--- application/views/admin/_footer.php
+++ application/views/admin/_footer.php
@@ -2,7 +2,6 @@
 				&copy; <?=date('Y')?> <?=$this->config->item('starter_copyright')?>
 			</div>
 		</div>
-		<script src="<?=base_url()?>/assets/shared/js/jquery-1.6.1.min.js"></script>
 		<script src="<?=base_url()?>/assets/shared/js/jquery.easing.1.3.js"></script>
 		<script src="<?=base_url()?>/assets/shared/js/jquery-ui-1.8.16.custom.min.js" type="text/javascript"></script>
 		<script src="<?=base_url()?>/assets/app/js/app.js"></script>
