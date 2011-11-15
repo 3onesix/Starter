65134d55f074389ea0fe3727c45aa4335818fb8e
James Finley <jamesfinley@James-Finleys-MacBook-Air.local>
2011-11-15 13:34:34 -0600

    Fixed a route.

--- application/config/routes.php
+++ application/config/routes.php
@@ -9,22 +9,22 @@ $route['admin/forgot_password/(:any)']	= "admin/account/action_forgot/$1";
 $route['admin/forgot_password_process'] = "admin/account/action_forgot_process";
 $route['admin/reset_password_process/(:any)'] = "admin/account/action_reset_process/$1";
 
-$route['admin/(:any)/destroy/(:any)']	= "admin/$1/action_destroy/$2";
-$route['admin/(:any)/update/(:num)']	= "admin/$1/action_update/$2";
-$route['admin/(:any)/edit/(:num)']		= "admin/$1/action_edit/$2";
-$route['admin/(:any)/create']	 		= "admin/$1/action_create";
-$route['admin/([^\/]*)/new']	 			= "admin/$1/action_new";
+$route['admin/([^\/]*)/destroy/(:any)']				= "admin/$1/action_destroy/$2";
+$route['admin/([^\/]*)/update/(:num)']				= "admin/$1/action_update/$2";
+$route['admin/([^\/]*)/edit/(:num)']				= "admin/$1/action_edit/$2";
+$route['admin/([^\/]*)/create']	 					= "admin/$1/action_create";
+$route['admin/([^\/]*)/new']	 					= "admin/$1/action_new";
 $route['admin/(:any)/(:any)/(:any)/(:any)/(:any)']	= "admin/$1/action_$2/$3/$4/$5";
-$route['admin/(:any)/(:any)/(:any)/(:any)']	= "admin/$1/action_$2/$3/$4";
-$route['admin/(:any)/(:any)/(:any)']	= "admin/$1/action_$2/$3";
-$route['admin/(:any)/(:any)']			= "admin/$1/action_$2";
-$route['admin/(:any)']					= "admin/$1/action_index";
-$route['admin']							= "admin/dashboard/action_index";
+$route['admin/(:any)/(:any)/(:any)/(:any)']			= "admin/$1/action_$2/$3/$4";
+$route['admin/(:any)/(:any)/(:any)']				= "admin/$1/action_$2/$3";
+$route['admin/(:any)/(:any)']						= "admin/$1/action_$2";
+$route['admin/(:any)']								= "admin/$1/action_index";
+$route['admin']										= "admin/dashboard/action_index";
 
-$route['migrations/create/(:any)']		= "migrations/create/$1";
-$route['migrations/seed']				= "migrations/seed";
-$route['migrations/reset']				= "migrations/reset";
-$route['migrations'] 					= 'migrations/index';
+$route['migrations/create/(:any)']					= "migrations/create/$1";
+$route['migrations/seed']							= "migrations/seed";
+$route['migrations/reset']							= "migrations/reset";
+$route['migrations'] 								= 'migrations/index';
 
 $modules = array();
 if (file_exists(MODPATH.'modules.php')) require(MODPATH.'modules.php');
