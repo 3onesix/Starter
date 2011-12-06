760748812e0210926cae38a7832a45499b7913c3
James Finley <jamesfinley@gmail.com>
2011-12-06 12:49:03 -0600

    Lowercased migrations.php. Frakkin' case sensitive file servers.

---
+++ application/controllers/migrations.php
@@ -0,0 +1,51 @@
+<?php
+
+class Migrations extends CI_Controller 
+{	
+	function __construct()
+	{
+		parent::__construct();
+
+		$this->load->database();
+		$this->load->helper('jot_migrations');	
+	}
+	
+	function index()
+	{
+		if (ENVIRONMENT == 'development' || $this->input->is_cli_request())
+		{
+			$migrations = new JotMigrations();
+			$migrations->up();
+		}
+		else
+		{
+			show_404();
+		}
+	}
+	
+	function reset()
+	{		
+		if (ENVIRONMENT == 'development' || IS_CLI)
+		{
+			$migrations = new JotMigrations();
+			$migrations->reset(TRUE);
+		}
+		else
+		{
+			show_404();
+		}
+	}
+	
+	function create($path)
+	{
+		if (ENVIRONMENT == 'development' || IS_CLI)
+		{
+			$migrations = new JotMigrations();
+			$migrations->create($path);
+		}
+		else
+		{
+			show_404();
+		}
+	}
+}
