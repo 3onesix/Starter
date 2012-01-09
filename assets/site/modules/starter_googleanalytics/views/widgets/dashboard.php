14a8319edb0234ff394c0ce4e741e0e8e9c3139e
James Finley <jamesfinley@gmail.com>
2012-01-09 08:56:32 -0600

    Only show widget if settings filled out for GA.

--- assets/site/modules/starter_googleanalytics/views/widgets/dashboard.php
+++ assets/site/modules/starter_googleanalytics/views/widgets/dashboard.php
@@ -1,3 +1,8 @@
+<?php 
+	$module = $this->module_model->first(array('simple_name' => 'starter_googleanalytics'));
+	if ($module->setting('username') && $module->setting('password') && $module->setting('profile')):
+?>
 <?php $this->load->helper('googleanalytics'); ?>
 <?php analytics_view('Site Stats', 'visits', array('range' => 'week')); ?>
 <?php analytics_view('Mobile Stats', 'mobile', array('range' => 'week')); ?>
+<?php endif; ?>
