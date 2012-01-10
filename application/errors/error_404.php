6267c48ae2f91f8f643b84935ab8737647e51b37
Tom Krush <t.krush@gmail.com>
2012-01-09 20:15:24 -0600

    Improved default 404 stylesheet.

--- application/errors/error_404.php
+++ application/errors/error_404.php
@@ -3,41 +3,34 @@
 if (file_exists('assets/site/templates/error_404.php'))
 {
 	include('assets/site/templates/error_404.php');
-	die();
+	return;
 }
+
+$CI =& get_instance();
+$hour = date('G');
+$CI->load->library('user_agent');
+
+$referral = $CI->agent->is_referral() ? ' <a href="'.$CI->agent->referrer().'">Go Back</a>' : NULL;
 
 ?>
+
+<!DOCTYPE html>
 <html>
-<head>
-<title>404 Page Not Found</title>
-<style type="text/css">
-
-body {
-background-color:	#fff;
-margin:				40px;
-font-family:		Lucida Grande, Verdana, Sans-serif;
-font-size:			12px;
-color:				#000;
-}
-
-#content  {
-border:				#999 1px solid;
-background-color:	#fff;
-padding:			20px 20px 12px 20px;
-}
-
-h1 {
-font-weight:		normal;
-font-size:			14px;
-color:				#990000;
-margin:				0 0 4px 0;
-}
-</style>
-</head>
-<body>
-	<div id="content">
-		<h1><?php echo $heading; ?></h1>
-		<?php echo $message; ?>
-	</div>
-</body>
+	<head>
+		<meta charset="utf-8"/>
+		<title><?=$CI->config->item('starter_product_name')?>: 404 (We're lost!)</title>
+		<!--[if lt IE 9]>
+			<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+		<!-- Adding "maximum-scale=1" fixes the Mobile Safari auto-zoom bug: http://filamentgroup.com/examples/iosScaleBug/ -->
+		
+		<link rel="stylesheet" href="<?=base_url()?>assets/app/css/errors.css" type="" />
+	</head>
+	<body>
+		<div id="content">
+			<h1 class="image" style="background-image: url(<?=site_url($CI->config->item('starter_product_image'))?>)"><a href="<?=site_url('admin')?>"><?=$CI->config->item('starter_product_name')?></a></h1>
+			<p>The requested URL <span class="url">/<?=uri_string()?></span> was not found on this server.<?=$referral?></p>
+		</div>
+	</body>
 </html>
