42fba1cdfcbf1a9af523c61b84f082566f811fd1
Tom Krush <t.krush@gmail.com>
2012-05-10 16:27:18 -0500

    Setup uploader

--- application/controllers/admin/images.php
+++ application/controllers/admin/images.php
@@ -4,6 +4,19 @@ class Images extends MY_Controller
 {
 	public function action_upload()
 	{
+		$image = new Image_Model;
+		$image->user_id = $this->current_user->id;
+		$image->save();
 		
+		$size = getimagesize($image->image->file_path());
+		
+		$output = array(
+			'id' => $image->id,
+			'width' => $size[0],
+			'height' => $size[1]
+		);
+		
+		$this->output->set_content_type('text/json');
+		$this->output->set_output(json_encode($output));
 	}
 }
