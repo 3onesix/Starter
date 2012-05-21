0f23dfd02ec48e563a5cd09b6504824b4b6b48cf
James Finley <jamesfinley@gmail.com>
2012-05-21 09:28:41 -0500

    Fixing a couple bugs with repositioning.

--- assets/app/variables/Image_Variable.php
+++ assets/app/variables/Image_Variable.php
@@ -12,14 +12,16 @@ class Image_Variable extends Starter_Variable {
 		$html  = '<div class="field">'."\n";
 		$html .= '<label for="'.$id.'">'.$this->variable->label.':</label>'."\n";
 		$html .= '<input type="file" name="'.$this->fieldname.'" id="'.$id.'" class="image-manager" data-width="'.$this->variable->options['width'].'" data-height="'.$this->variable->options['height'].'" />'."\n";
+		
+		$image = $this->load();
+		if ($image) $html .= '<img src="'.$image.'" />'."\n";
+		
 		$html .= '</div>'."\n";
 		$html .= '<script type="text/javascript">'."\n";
 		$html .= 'if ($("head script[src*=\'image_manager.js\']").size() == 0) {'."\n";
 		$html .= '$.getScript("/assets/app/js/image_manager.js")'."\n";
 		$html .= '}'."\n";
 		$html .= '</script>'."\n";
-		$image = $this->load();
-		if ($image) $html .= '<img src="'.$image.'" />';
 		return $html;
 	}
 	
