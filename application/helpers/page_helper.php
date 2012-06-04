f555e6de771579d0caacaaa9b6d06996bcd2de46
Tom Krush <t.krush@gmail.com>
2012-06-04 15:33:44 -0500

    Fixed page helper

--- application/helpers/page_helper.php
+++ application/helpers/page_helper.php
@@ -1,14 +1,16 @@
 <?php
 
-function include_template($template)
+function include_template($template, $data = array())
 {
 	if (file_exists('assets/site/templates/'.$template.'.php'))
 	{
+		extract($data);
+		
 		include('assets/site/templates/'.$template.'.php');
 	}
 }
 
-function include_partial($template)
+function include_partial($template, $data)
 {
-	include_template('_'.$template);
+	include_template('_'.$template, $data = array());
 }
