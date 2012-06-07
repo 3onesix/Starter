2efeaaffae4677b533d2ce09b6a7d9e387529db8
Tom Krush <t.krush@gmail.com>
2012-06-07 09:13:11 -0500

    Added template class

--- application/helpers/page_helper.php
+++ application/helpers/page_helper.php
@@ -1,29 +1,64 @@
 <?php
 
-function include_template($template, $data = array())
+function include_template($name, $data = array())
 {
-	if (file_exists('assets/site/templates/'.$template.'.php'))
-	{		
-		$CI =& get_instance();
+	$template = new Template($name, $data);
+	$template->render();
+}
+
+function include_partial($name, $data = array())
+{
+	include_template('_'.$name, $data = array());
+}
+
+class Template
+{
+	protected $name = '';
+	protected $data = array();
 	
-		if ( is_array($CI->variables) )
-		{
-			foreach ($CI->variables as $k => $v)
+	public function __construct($name, $data = array())
+	{
+		$this->name = $name;
+		$this->data = $data;
+	}
+	
+	public function path()
+	{
+		return 'assets/site/templates/'.$this->name.'.php';
+	}
+	
+	public function render()
+	{
+		if (file_exists($this->path()))
+		{		
+			$CI =& get_instance();
+		
+			if ( is_array($CI->variables) )
+			{
+				foreach ($CI->variables as $k => $v)
+				{
+					$$k = $v;
+				}
+			}
+			
+			foreach ($this->data as $k => $v)
 			{
 				$$k = $v;
 			}
+			
+			include($this->path());
 		}
-		
-		foreach ($data as $k => $v)
-		{
-			$$k = $v;
-		}
-		
-		include('assets/site/templates/'.$template.'.php');
 	}
-}
-
-function include_partial($template, $data = array())
-{
-	include_template('_'.$template, $data = array());
+	
+	public function __get($key)
+	{
+		$CI =& get_instance();
+		
+		if ( property_exists($CI, $key) )
+		{
+			return $CI->$key;
+		}
+		
+		return false;
+	}
 }
